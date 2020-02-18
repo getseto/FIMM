8cec99616a99ca8bcc1b3f887b7f334869ffd12d
Getse <getsee.to@gmail.com>
2020-02-18 10:30:12 -0600

    Busqueda funcionando cuando se edita

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -50,13 +50,13 @@ export const getAssistantsForEvent = async (firebaseApp, searchTerm = '', eventI
     let query = firebase.firestore().collection('/event').doc(eventId).collection('assistants')
     let snapshot
     if (searchTerm) {
-        const data = (await query.where('firstName', '==', searchTerm).get()).docs.map(doc => doc.data())
-        data.push(...(await query.where('lastName1', '==', searchTerm).get()).docs.map(doc => doc.data()))
-        data.push(...(await query.where('lastName2', '==', searchTerm).get()).docs.map(doc => doc.data()))
-        data.push(...(await query.where('curp', '==', searchTerm).get()).docs.map(doc => doc.data()))
-        data.push(...(await query.where('email', '==', searchTerm).get()).docs.map(doc => doc.data()))
-        data.push(...(await query.where('phone', '==', searchTerm).get()).docs.map(doc => doc.data()))
-        data.push(...(await query.where('cellphone', '==', searchTerm).get()).docs.map(doc => doc.data()))
+        const data = (await query.where('firstName', '==', searchTerm).get()).docs.map(doc => ({...doc.data(), id: doc.id}))
+        data.push(...(await query.where('lastName1', '==', searchTerm).get()).docs.map(doc => ({...doc.data(), id: doc.id})))
+        data.push(...(await query.where('lastName2', '==', searchTerm).get()).docs.map(doc => ({...doc.data(), id: doc.id})))
+        data.push(...(await query.where('curp', '==', searchTerm).get()).docs.map(doc => ({...doc.data(), id: doc.id})))
+        data.push(...(await query.where('email', '==', searchTerm).get()).docs.map(doc => ({...doc.data(), id: doc.id})))
+        data.push(...(await query.where('phone', '==', searchTerm).get()).docs.map(doc => ({...doc.data(), id: doc.id})))
+        data.push(...(await query.where('cellphone', '==', searchTerm).get()).docs.map(doc => ({...doc.data(), id: doc.id})))
         return data;
     } else {
         snapshot = await query.get()
